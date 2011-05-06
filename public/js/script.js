55c0fc04467fea31ef0565e660f6375ef20323fc
Dmitry Groza <boxfrommars@gmail.com>
2011-05-07 02:00:53 +0400

    terminal added to page

--- public/js/script.js
+++ public/js/script.js
@@ -2,42 +2,106 @@ document.createElement('header');
 document.createElement('footer');
 
 $(document).ready(function(){
-	var cellSize = { 'width' : 34, 'height' : 23 };
-	var fieldSize = { 'width' : $('body').width(), 'height' : $('body').height() };
+	// настройки
+	var cellSize = { 'width' : 34, 'height' : 23 };	// ширина картинки единорога
+	var blocks = {'width' : 10, 'height' : 6, 'total' : this.width * this.height} 		// на сколько блоков делим
+	
+	
+
+	var fieldSize = { 'width' : $('body').width(), 'height' : $('body').height() }; 
+	var blockSize = {
+			'width' : ~~(fieldSize.width / (cellSize.width * blocks.width)), 
+			'height' : ~~(fieldSize.height / (cellSize.height * blocks.height))
+	};
+	console.log(blockSize);
+	var getRandomBlockCell = function(){
+		return {
+			x : ~~(Math.random() * (blockSize.width)),
+			y : ~~(Math.random() * (blockSize.height))
+		} 
+	}
+
+	
+	var deadFields = []; // тут будем хранить координаты блоков, где не могут быть клетки
+	$('.no-cell').each(function(i){
+		
+		var dimension = { 'width' : $(this).outerWidth(), 'height' : $(this).outerHeight() }
+		var offset = $(this).offset();
+		
+		// ну ещё с бордюрчиком + 1 клетка
+		deadFields.push({
+			'left' : ~~(offset.left / cellSize.width),
+			'right' : ~~((offset.left + dimension.width) / cellSize.width),
+			'top' : ~~(offset.top / cellSize.height),
+			'bottom' : ~~((offset.top + dimension.height) / cellSize.height)
+		});
+	});
+//	var Field = {
+//			cellSize : { 'width' : 34, 'height' : 23 },
+//			fieldSize : { 'width' : $('body').width(), 'height' : $('body').height() },
+//			deadFields : [],
+//			
+//			init : function() {
+//				
+//			},
+//			
+//			addWish : function(wish) {
+//				
+//			},
+//			
+//			getPosition : function(wish) {
+//				
+//			}
+//			
+//	}
 	
 	var Wish = function(data){
 		this.data = data;
 	};
 	
+	var inDeadFields = function(pos) {
+		var inDead = false;
+		$.each(deadFields, function(i, field) {
+			if ((pos.x >= field.left) && (pos.x <= field.right) && (pos.y >= field.top) && (pos.y <= field.bottom)) {
+				inDead = true;
+				console.log('hey!')
+				return false;
+			};
+		});
+		return inDead;
+	}
+	
+	var isCorrectCell = function(pos) {
+		return !inDeadFields(pos);
+	}
+	
+	var findWishCoords = function(wish) {
+		var fieldBlockId = wish.getPosition();
+		var wishBlock = {
+				x : fieldBlockId % blocks.width,
+				y : ~~(fieldBlockId / blocks.width)
+		}
+		var blockCell = getRandomBlockCell();
+		
+		var cell = {x : (wishBlock.x * blockSize.width) + blockCell.x, y : (wishBlock.y * blockSize.height) + blockCell.y};
+		return isCorrectCell(cell) ? cell : findWishCoords(wish);
+		return cell;
+	}
+	
 	Wish.prototype = {
 		getName : function() { return this.data.name; },
 		getText : function() { return this.data.text; },
-		getPosition : function() { return {x : 10, y : 10}; },
+		getPosition : function() { return this.data.position; },
 		getBubbleHtml : function() { return '<strong>' + this.getName() + '</strong><p>' + this.getText() + '</p>'; },
 	}
 	
 	var addWish = function(wish) {
-		var pos = wish.getPosition();
+		var wishCoords = findWishCoords(wish);
 		$('<div>').addClass('cell')
 		          .data({'wish' : wish})
-		          .css({'top' : cellSize.height * pos.y, 'left' : cellSize.width * pos.x})
+		          .css({'top' : cellSize.height * wishCoords.y, 'left' : cellSize.width * wishCoords.x})
 		          .appendTo($('body'));
 	};
-	
-	var deadFields = []; // тут будем хранить координаты блоков, где не могут быть клетки
-	$('.no-cell').each(function(i){
-		
-		var dimension = { 'width' : $(this).width(), 'height' : $(this).height() }
-		var offset = $(this).offset();
-		
-		// ну ещё с бордюрчиком + 1 клетка
-		deadFields.push({
-			'left' : ~~(offset.left / cellSize.width) - 1,
-			'right' : ~~((offset.left + dimension.width) / cellSize.width) + 2,
-			'top' : ~~(offset.top / cellSize.height) - 1,
-			'bottom' : ~~((offset.top + dimension.height) / cellSize.height) + 2
-		});
-	});
 	
 	$.each(wishes, function(i, wishData){
 		wish = new Wish(wishData);
@@ -58,4 +122,12 @@ $(document).ready(function(){
 			'closingDelay' : 200
 		});
 	});
+	
+
+//    $('#terminal').terminal("/rpc", {
+//        login: true,
+//        greetings: "You are authenticated"
+//    });
+	
+	
 });
